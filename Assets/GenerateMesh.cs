bdae02e0eeddbe8b37a6666ca46b476619ecefcd
jebbyk <zaicev9797@gmail.com>
2019-04-03 02:10:46 +0600

    initial commit
    -generating plane

---
+++ Assets/GenerateMesh.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class GenerateMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Mesh mesh;
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+
+    Vector3[] vertices;
+    int[] tris;
+
+    Vector3Int[] trisCombined;
+
+    struct poly{
+
+    }
+
+
+    void Start()
+    {
+        mesh = new Mesh();
+        meshFilter = GetComponent<MeshFilter>();
+        vertices = new Vector3[]{
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0),
+            new Vector3(1,0,1),
+        };
+
+        tris = new int[]{
+            0,1,2,
+            1,3,2
+        };
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        meshFilter.mesh = mesh;
+    }
+    void Update()
+    {
+        
+    }
+}
